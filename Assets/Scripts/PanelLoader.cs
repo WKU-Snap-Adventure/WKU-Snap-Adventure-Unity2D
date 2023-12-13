c5ea400660f2f6bf5bf27d88357c9ce935df8f91
秦钰奇 <121993898+APieceOfToast123@users.noreply.github.com>
2023-12-13 17:28:10 +0800

    Fixed the bug of overflow trigger

--- Assets/Scripts/PanelLoader.cs
+++ Assets/Scripts/PanelLoader.cs
@@ -1,37 +1,65 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelLoader : MonoBehaviour
 {
     public GameObject LoadedPanel;
     public Animator LoadedPanelAnimator;
     public float transitionTime = 2.5f;
+    private Button iconButton;
+    private Button quitButton;
+
     // Start is called before the first frame update
     void Start()
     {
         LoadedPanel.SetActive(false);
+
+        iconButton = transform.parent.GetComponent<Button>();
+        Transform quit = transform.parent.Find("Quit");
+
+        if (quit != null)
+        {
+            quitButton = quit.GetComponent<Button>();
+        }
     }
 
-    public void LoadPanel(){
+    public void LoadPanel()
+    {
+        iconButton.transition = Selectable.Transition.None;
         LoadedPanel.SetActive(true);
-        
         LoadedPanelAnimator.SetTrigger("LoadPanel");
+        StartCoroutine(WaitForLoadPanelAnimation());
     }
 
-    public void ClosePanel(){        
-        LoadedPanelAnimator.SetTrigger("ClosePanel");
+    IEnumerator WaitForLoadPanelAnimation()
+    {
+        yield return new WaitForSeconds(transitionTime);
 
+        if (quitButton != null)
+        {
+            quitButton.interactable = true;
+        }
+
+        iconButton.transition = Selectable.Transition.ColorTint;
+    }
+
+    public void ClosePanel()
+    {
+        if (quitButton != null)
+        {
+            quitButton.interactable = false;
+        }
+
+        LoadedPanelAnimator.SetTrigger("ClosePanel");
         StartCoroutine(WaitForClosePanelAnimationAndClosePanel());
     }
 
-    IEnumerator WaitForClosePanelAnimationAndClosePanel(){
-
+    IEnumerator WaitForClosePanelAnimationAndClosePanel()
+    {
         yield return new WaitForSeconds(transitionTime);
 
         LoadedPanel.SetActive(false);
+
     }
-
-
 }
