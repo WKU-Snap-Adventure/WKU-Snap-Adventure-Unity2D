400187f4b8a4f8e15bc682269cbb723387e2ad6c
秦钰奇 <121993898+APieceOfToast123@users.noreply.github.com>
2024-01-25 19:27:17 +0800

    No need of this time manager version

--- Assets/Scripts/ClockManager.cs
+++ Assets/Scripts/ClockManager.cs
@@ -1,56 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using DPUtils.Systems.DataTime;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
+// using DPUtils.Systems.DataTime;
 
-public class ClockManager : MonoBehaviour
-{
-    public RectTransform ClockFace;
-    public TextMeshProUGUI Date, Time, Season, Week;
+// public class ClockManager : MonoBehaviour
+// {
+//     public RectTransform ClockFace;
+//     public TextMeshProUGUI Date, Time, Season, Week;
 
-    public Image weatherSprite;
-    public Sprite[] weatherSprites;
+//     public Image weatherSprite;
+//     public Sprite[] weatherSprites;
 
-    private float startingRotation;
+//     private float startingRotation;
 
-    public Light sunlight;
-    public float nightIntensity;
-    public float dayIntensity;
+//     public Light sunlight;
+//     public float nightIntensity;
+//     public float dayIntensity;
 
-    public AnimationCurve dayNightCurve;
+//     public AnimationCurve dayNightCurve;
 
-    private void Awake()
-    {
-        startingRotation = ClockFace.localEulerAngles.z;
-    }
+//     private void Awake()
+//     {
+//         startingRotation = ClockFace.localEulerAngles.z;
+//     }
 
-    private void OnEnable()
-    {
-        TimeManager.OnDateTimeChanged += UpdateDateTime;
-    }
+//     private void OnEnable()
+//     {
+//         TimeManager.OnDateTimeChanged += UpdateDateTime;
+//     }
 
-    private void OnDisable()
-    {
-        TimeManager.OnDateTimeChanged -= UpdateDateTime;
-    }
+//     private void OnDisable()
+//     {
+//         TimeManager.OnDateTimeChanged -= UpdateDateTime;
+//     }
 
-    private void UpdateDateTime(DateTime dateTime)
-    {
-        Date.text = dateTime.DateToString();
-        Time.text = dateTime.TimeToString();
-        Season.text = dateTime.SeasonToString();
-        Week.text = $"WK: {dateTime.CurrentWeek.ToString()}";
-        // weatherSprite.sprite = weatherSprites[(int)WeatherManager.currentWeather];
+//     private void UpdateDateTime(DateTime dateTime)
+//     {
+//         Date.text = dateTime.DateToString();
+//         Time.text = dateTime.TimeToString();
+//         Season.text = dateTime.SeasonToString();
+//         Week.text = $"WK: {dateTime.CurrentWeek.ToString()}";
+//         // weatherSprite.sprite = weatherSprites[(int)WeatherManager.currentWeather];
 
-        float t = (float)dateTime.Hour / 24f;
+//         float t = (float)dateTime.Hour / 24f;
 
-        float newRotation = Mathf.Lerp(0, 360, t);
-        ClockFace.localEulerAngles = new Vector3(0, 0, newRotation + startingRotation);
+//         float newRotation = Mathf.Lerp(0, 360, t);
+//         ClockFace.localEulerAngles = new Vector3(0, 0, newRotation + startingRotation);
 
-        float dayNightT = dayNightCurve.Evaluate(t);
+//         float dayNightT = dayNightCurve.Evaluate(t);
 
-        sunlight.intensity = Mathf.Lerp(nightIntensity, dayIntensity, dayNightT);
-    }
-}
+//         sunlight.intensity = Mathf.Lerp(nightIntensity, dayIntensity, dayNightT);
+//     }
+// }
